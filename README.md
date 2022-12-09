283da7fe5bc131a541bd02678fb435aabfda7923
psj0606 <psjchoigo06@gmail.com>
2022-12-09 19:52:07 +0900

    1

---
+++ README.md
@@ -0,0 +1,3 @@
+## 6. SEO
+
+- [블로그 글 링크](https://miryang.dev/blog/build-blog-with-nextjs#6%EF%B8%8F%E2%83%A3-6-seo)
---
+++ data/metadata.ts
@@ -0,0 +1,8 @@
+const metadata = {
+  title: 'MOVIE',
+  description: "who's blog",
+  author: 'MiryangJung',
+  name: '성진',
+};
+
+export default metadata;
